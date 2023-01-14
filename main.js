6abb10e46528a49f7e56b1bda1ad058cd56a35c4
Assabri <121148484+Assabri@users.noreply.github.com>
2023-01-14 16:01:34 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,225 @@
+// menu item start
+
+
+
+
+$('.fa-times').hide();
+$('.mobile-menu-item').hide();
+
+
+$('.fa-align-justify').click(function(){
+	$('.fa-align-justify').hide()
+	$('.fa-times').show()
+	$('.mobile-menu-item').slideDown()
+
+});
+
+
+
+$('.fa-times').click(function(){
+	$('.fa-align-justify').show()
+	$('.fa-times').hide()
+	$('.mobile-menu-item').slideUp()
+
+});
+
+
+
+		// menu item close
+
+
+
+
+
+		// responsive menu start
+
+
+
+$('.service-up').hide();
+$('.web-up').hide();
+
+
+
+$('.service-drop').hide();
+$('.web-drop').hide();
+
+
+
+
+
+$('.service-down').click(function(){
+
+	$('.service-up').show()
+	$('.service-down').hide()
+	$('.service-drop').slideDown(1000)
+
+});
+
+$('.service-up').click(function(){
+
+	$('.service-down').show()
+	$('.service-up').hide()
+	$('.service-drop').slideUp(1000)
+
+});
+
+
+
+
+
+
+
+
+
+
+$('.web-down').click(function(){
+	$('.web-up').show()
+	$('.web-down').hide()
+	$('.web-drop').slideDown(1000)
+
+});
+
+$('.web-up').click(function(){
+	$('.web-down').show()
+	$('.web-up').hide()
+	$('.web-drop').slideUp()
+
+});
+
+
+
+
+
+		// responsive menu close
+
+
+
+
+
+
+
+
+		// faq section start
+
+
+
+
+
+
+
+
+$('.fa-minus').hide();
+$('.ans').hide();
+
+
+
+$('.plus1').click(function(){
+	$('.minus1').show()
+	$('.plus1').hide()
+	$('.ans1').slideDown()
+
+});
+
+$('.minus1').click(function(){
+	$('.minus1').hide()
+	$('.plus1').show()
+	$('.ans1').slideUp()
+
+});
+
+
+
+
+
+
+$('.plus2').click(function(){
+	$('.minus2').show()
+	$('.plus2').hide()
+	$('.ans2').slideDown()
+
+});
+
+$('.minus2').click(function(){
+	$('.minus2').hide()
+	$('.plus2').show()
+	$('.ans2').slideUp()
+
+});
+
+
+
+
+
+$('.plus3').click(function(){
+	$('.minus3').show()
+	$('.plus3').hide()
+	$('.ans3').slideDown()
+
+});
+
+$('.minus3').click(function(){
+	$('.minus3').hide()
+	$('.plus3').show()
+	$('.ans3').slideUp()
+
+});
+
+
+
+
+
+$('.plus4').click(function(){
+	$('.minus4').show()
+	$('.plus4').hide()
+	$('.ans4').slideDown()
+
+});
+
+$('.minus4').click(function(){
+	$('.minus4').hide()
+	$('.plus4').show()
+	$('.ans4').slideUp()
+
+});
+
+
+
+
+
+$('.plus5').click(function(){
+	$('.minus5').show()
+	$('.plus5').hide()
+	$('.ans5').slideDown()
+
+});
+
+$('.minus5').click(function(){
+	$('.minus5').hide()
+	$('.plus5').show()
+	$('.ans5').slideUp()
+
+});
+
+
+
+
+
+$('.plus6').click(function(){
+	$('.minus6').show()
+	$('.plus6').hide()
+	$('.ans6').slideDown()
+
+});
+
+$('.minus6').click(function(){
+	$('.minus6').hide()
+	$('.plus6').show()
+	$('.ans6').slideUp()
+
+});
+
+
+
+
+
+
